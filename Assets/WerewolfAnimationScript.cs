a4b57b59ddd3142695d6b2d05918c47e323ec290
Unknown <C0zz4a@hotmail.co.uk>
2019-03-13 21:03:40 +0000

    Werewolf Model with animations
    Made an extra scene for me to test stuff since I kept messing with the main scene in AE1. Test if yooos want

---
+++ Assets/WerewolfAnimationScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WerewolfAnimationScript : MonoBehaviour {
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetBool("IsRunning", false);
+            anim.SetBool("IsWalking", true);
+        } else if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            anim.SetBool("IsWalking", false);
+            anim.SetBool("IsRunning", true);
+           
+        } else if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            anim.SetBool("IsWalking", false);
+            anim.SetBool("IsRunning", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetTrigger("Death");
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            anim.SetTrigger("Roar");
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            anim.SetTrigger("Attack");
+        }
+	}
+}
