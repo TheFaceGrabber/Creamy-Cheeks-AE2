243b2c4ea3489ee1d001d9c339fea33323c58659
Unknown <C0zz4a@hotmail.co.uk>
2019-04-11 20:34:39 +0100

    Master Bedroom Cinematic
    Attempt 2

---
+++ Assets/TimelineScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class TimelineScript : MonoBehaviour {
+
+    PlayableDirector director;
+
+    public bool startedOnCollision;
+
+    private void Awake()
+    {
+        director = GetComponent<PlayableDirector>();
+    }
+
+    private void OnTriggerEnter(Collider col)   // If the cinematic is played by
+    {                                           // colliding with something, e.g. 
+        if (startedOnCollision)                 // walking into a room, do this
+        {
+            if (col.tag == "Player")
+            {
+                director.Play();
+            }
+        }
+    }
+
+    public void Play()  // When the cinematic needs to be manually called upon some kind of event
+    {
+        director.Play();
+    }
+}
