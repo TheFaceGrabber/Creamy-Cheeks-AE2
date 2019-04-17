e3e6a87eb65640d63370f36a5dee54794790114b
Joe Constable <joeconstable97@gmail.com>
2019-04-17 10:58:40 +0100

    Lunar room cinematic (untested)

---
+++ Assets/Scripts/LunarRoomEvent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class LunarRoomEvent : MonoBehaviour
+{
+	public PlayableDirector Cinematic;
+	public GameObject Priest;
+	GameObject Loui;
+	public GameObject LouiWolf;
+	
+	public GameObject Werewolf;
+	
+	// Use this for initialization
+	public void StartEvent ()
+	{
+		Loui = GameObject.Find("Loui");
+		
+		StartCoroutine(BeginEvent());
+	}
+
+	IEnumerator BeginEvent()
+	{
+		Loui.SetActive(false);
+		LouiWolf.SetActive(true);
+		
+		Cinematic.Play();
+		
+		yield return new WaitForSeconds(91);
+		
+		Priest.SetActive(false);
+		GameObject i = Instantiate(Werewolf, Priest.transform.position, Priest.transform.rotation);
+	}
+}
